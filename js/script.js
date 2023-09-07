26a4230beb51fd89856791a6539d734751d6af87
edy2209 <edisyafrianto45@gmail.com>
2023-09-07 18:16:16 +0700

    piza api

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$.getJSON('data/pizza.json',function(data){
+    let menu = data.menu;
+    $.each(menu,function(i,data){
+        $('#daftar-menu').append('<div class="col-md-4"><div class="card mb-3"><img src="img/pizza/'+ data.gambar +'" class="card-img-top"><div class="card-body"><h5 class="card-title">' +data.nama+ '</h5><p class="card-text">'+ data.deskripsi+ '</p><h5 class="card-title">Rp.'+ data.harga +'</h5><a href="#" class="btn btn-primary">pesan sekarang</a></div></div></div>')
+
+
+    });
+});
+
+$('.nav-link').on('click',function(){
+    $('.nav-link').removeClass('active');
+    $(this).addClass('active');
+});
